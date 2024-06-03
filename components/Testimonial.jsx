0cdbe85bdb6aabbd7a2ca80a749db2c562334d63
Yashgupta9330 <yaahg342@gmail.com>
2024-06-03 18:57:54 +0530

    added

--- components/Testimonial.jsx
+++ components/Testimonial.jsx
@@ -20,7 +20,7 @@ export default function Testimonials() {
   useEffect(() => {
     let slider = setInterval(() => {
       setIndex(index + 1);
-    }, 5000);
+    }, 2000);
     return () => clearInterval(slider);
   }, [index]);
 
