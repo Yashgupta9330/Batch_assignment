7bb8b9e249c627176e9415e53c9de677dc258043
Yashgupta9330 <yaahg342@gmail.com>
2024-06-03 18:55:54 +0530

    added

--- components/Latest.jsx
+++ components/Latest.jsx
@@ -3,72 +3,16 @@ import phone from "../public/phone.png";
 import Group12 from "../public/Group359241.png";
 import vector1 from "../public/Vector1.png";
 import Vector2 from "../public/Vector2.png";
-import "./Main.css";
+import "./Latest.css";
 import Image from "next/image";
 import Button from "./Button";
+import Back from "./Back";
 export default function Heading1(){
   return (
     <div className="main">
     <div className="ecli-main mt-2 d-none d-lg-block d-md-block">
-    <svg
-      width="517"
-      height="430"
-      viewBox="0 0 517 430"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g filter="url(#filter0_f_101_132)">
-        <ellipse cx="245" cy="253.5" rx="145" ry="76.5" fill="#FDE598" />
-      </g>
-      <g filter="url(#filter1_f_101_132)">
-        <ellipse cx="275" cy="176.5" rx="142" ry="76.5" fill="#FF5555" />
-      </g>
-      <defs>
-        <filter
-          id="filter0_f_101_132"
-          x="0"
-          y="77"
-          width="490"
-          height="353"
-          filterUnits="userSpaceOnUse"
-          colorInterpolationFilters="sRGB"
-        >
-          <feFlood floodOpacity="0" result="BackgroundImageFix" />
-          <feBlend
-            mode="normal"
-            in="SourceGraphic"
-            in2="BackgroundImageFix"
-            result="shape"
-          />
-          <feGaussianBlur
-            stdDeviation="50"
-            result="effect1_foregroundBlur_101_132"
-          />
-        </filter>
-        <filter
-          id="filter1_f_101_132"
-          x="33"
-          y="0"
-          width="484"
-          height="353"
-          filterUnits="userSpaceOnUse"
-          colorInterpolationFilters="sRGB"
-        >
-          <feFlood floodOpacity="0" result="BackgroundImageFix" />
-          <feBlend
-            mode="normal"
-            in="SourceGraphic"
-            in2="BackgroundImageFix"
-            result="shape"
-          />
-          <feGaussianBlur
-            stdDeviation="50"
-            result="effect1_foregroundBlur_101_132"
-          />
-        </filter>
-      </defs>
-    </svg>
-  </div>
+     <Back/>
+    </div>
    <div className="container mx-auto">
    <div className="flex flex-col lg:flex-row gap-5">
     <div className=" flex flex-col items-center justify-center lg:w-1/2 mx-5">
@@ -79,7 +23,7 @@ export default function Heading1(){
             Cum et convallis risus placerat aliquam, nunc. Scelerisque aliquet
             faucibus tincidunt eu adipiscing sociis arcu lorem porttitor.
         </p>
-        <div className="twobtn flex items-center justify-start gap-4 lg:gap-24" style={{display:"flex",alignItems:"center"}}>
+        <div className="flex items-center w-full justify-start mt-4 gap-4 lg:gap-24">
         <Button text="Get Started" icon={vector1} />
         <div className="flex gap-2 items-center">
           <div className="border rounded-[100%] border-black flex items-center justify-center w-[29px] h-[29px]">
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,13 +4,16 @@ import { useState } from "react";
 import Button from "./Button";
 import { items } from "@/utils/Items";
 import Image from "next/image";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 export default function Navbar(){
     const [selected,setSelected]=useState("Home");
+    const [nav, setNav] = useState(false);
     return(
         <div className="flex items-center justify-center w-[92%] lg:w-4/5 h-[100px]">
         <nav className="flex w-full border items-center justify-between">
           <div className="flex items-center justify-center gap-6">
             <Image src={Group} alt="logo" className="cursor-pointer" width={120}/>
+            <div className="hidden md:block">
             <ul className="flex items-center justify-center gap-[24px]">
               {items.map((item, idx) => (
                 <li
@@ -23,8 +26,28 @@ export default function Navbar(){
               ))}
             </ul>
           </div>
+          </div>
           <Button  text="submit"/>
-        </nav>
+          <div onClick={() => setNav(!nav)} className="block md:hidden">
+              {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+            </div>
+            <ul
+              className={
+                nav
+                  ? "fixed md:hidden left-0 top-0 w-[60%] h-screen border-r text-white border-r-gray-900 bg-[#FF5555]  ease-in-out duration-500 z-50"
+                  : "ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]"
+              }
+            >
+              {items.map((item) => (
+                <li
+                  key={item}
+                  className="p-4 border-b rounded-xl hover:bg-[#cccccc] hover:text-black duration-300 cursor-pointer border-gray-600"
+                >
+                  {item}
+                </li>
+              ))}
+            </ul>
+         </nav>
       </div>
     )
 }
